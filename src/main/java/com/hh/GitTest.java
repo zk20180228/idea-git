78541abf98fc9f710a3f5a4b3f9eeaec9c9c1c71
zk <1589022273@qq.com>
2021-11-16 11:10:08 +0800

    git third commit

--- src/main/java/com/hh/GitTest.java
+++ src/main/java/com/hh/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git very good !");
         System.out.println("hello git !");
+        System.out.println(System.currentTimeMillis());
     }
 
 }
