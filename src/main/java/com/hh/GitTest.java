3c0a79e7fbd7c548415a851e26623e8e1ffb397a
zk <1589022273@qq.com>
2021-11-16 15:02:09 +0800

    Revert "git third commit"
    This reverts commit 78541abf

--- src/main/java/com/hh/GitTest.java
+++ src/main/java/com/hh/GitTest.java
@@ -12,14 +12,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git very good !");
         System.out.println("hello git !");
-        System.out.println(System.currentTimeMillis());
-        System.out.println(System.currentTimeMillis());
-        System.out.println(System.currentTimeMillis());
-        System.out.println(System.currentTimeMillis());
-        System.out.println(System.currentTimeMillis());
-        System.out.println(System.currentTimeMillis());
-        System.out.println("000000000000000");
-        System.out.println("111111111111111");
     }
 
 }
