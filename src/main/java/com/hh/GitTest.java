90fc8e67a983d743be9dc12e42ed28b1b8505d5d
zk <1589022273@qq.com>
2021-11-16 10:59:49 +0800

    git first commit

---
+++ src/main/java/com/hh/GitTest.java
@@ -0,0 +1,18 @@
+package com.hh;
+
+/**
+ * @Author: zk
+ * @Date: 2021/11/16 10:49
+ * @Description:
+ * @Modified:
+ * @version: V1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("git very good !");
+        System.out.println("hello git !");
+        System.out.println("hello git !");
+    }
+
+}
